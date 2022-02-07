623ba52165e62e743b18e251e49ebb92643ed9d9
thomas10-10 <36126914+thomas10-10@users.noreply.github.com>
2022-02-07 18:44:22 +0100

    Update lock.sh

--- lock.sh
+++ lock.sh
@@ -1,3 +1,4 @@
 #!/bin/env bash
-touch ~/.lock2.png; rm -f ~/.lock2.png ; scrot ~/.lock.png ; convert ~/.lock.png -scale 10% -scale 1000% ~/.lock2.png ; i3lock -u -i ~/.lock2.png
-
+x=10 ; y=1000
+[[ "$1" == "hard" ]] && { x=5 ; y=2000 ; }
+touch ~/.lock2.png; rm -f ~/.lock2.png ; scrot ~/.lock.png ; convert ~/.lock.png -scale ${x}% -scale ${y}% ~/.lock2.png; i3lock -u -i ~/.lock2.png
